8d27469dfd1199e18ca14436ddff3479f93d4064
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-01-06 08:37:25 +0300

    some minor style fixes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
-from circleshape import *
-from constants import PLAYER_RADIUS
+import pygame
+from constants import *
+from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x, y):
