23165a904b813df09a53f967813c3e6b7acab523
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-01-06 09:21:25 +0300

    some style fixes

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
     dt = 0
 
