aa9e89f4e8f148bd886de054da86855de905df11
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-01-06 09:01:47 +0300

    added player movement

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill(000000)
+            
         player.update(dt)
+        
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
